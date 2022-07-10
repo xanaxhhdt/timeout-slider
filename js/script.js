48eb1a2c6558cd89698f84ba0d2543cdd6c7b8e2
Vladislav <nicochan00@mail.ru>
2022-07-10 04:03:34 +0300

    final setTimeout Slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const slide = document.querySelectorAll('.slide');
+
+let time = 2000;
+let activeSlide = 3;
+slide[activeSlide].classList.add('slide_active');
+
+function changeSlide() {
+   if (activeSlide < slide.length - 1) {
+      slide[activeSlide].classList.remove('slide_active');
+      activeSlide++;
+      slide[activeSlide].classList.add('slide_active');
+   } else {
+      slide[activeSlide].classList.remove('slide_active');
+      activeSlide = 0;
+      slide[activeSlide].classList.add('slide_active');
+   }
+   setTimeout(changeSlide, time);
+}
+changeSlide();
